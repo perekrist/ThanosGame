3087fc1f7b9b34cbf9874020949ed13d9c21f1cb
perekrist <perekrist31@gmail.com>
2019-04-11 21:11:54 +0700

    Initial Commit

---
+++ ThanosGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThanosGame
+//
+//  Created by Перегудова Кристина on 11/04/2019.
+//  Copyright © 2019 HITs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
